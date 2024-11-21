0b7bf6f8d69c96b963dabeaf32f6bdb29927dc83
shawaeon <jens.sjogr@gmail.com>
2024-11-21 18:45:25 +0200

    add player object to groups

--- main.py
+++ main.py
@@ -8,22 +8,34 @@ from constants import *
 def main():
     pygame.init()    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    player = Player(x=(SCREEN_WIDTH / 2), y=(SCREEN_HEIGHT / 2))
+    clock = pygame.time.Clock() 
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x=(SCREEN_WIDTH / 2), y=(SCREEN_HEIGHT / 2))
+
 
     while True:  
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return   
-             
-        screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in updatable:
+            obj.update(dt)        
+
+        screen.fill("black")        
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         
         # Framerate limited to 60 FPS        
-        dt = (clock.tick(60) / 1000)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -6,11 +6,15 @@ from constants import *
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation = 180
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -29,3 +33,7 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
